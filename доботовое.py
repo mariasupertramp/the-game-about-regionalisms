f1df3d2e70eee0512bf82b6987f1abe743eaf904
vargnorsk <107671948+vargnorsk@users.noreply.github.com>
2025-05-18 22:44:57 +0300

    Add files via upload
    логика игры, пока не интегрированная в бота. 
    выкачка данных из csv для дальнейшей работы (черновой вариант)

---
+++ доботовое.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from json import loads, dumps
+import random
+
+
+data = pd.read_csv("регионализмы_разделённые.tsv", sep='\t')
+tojson = data.to_json(orient="records")
+parsed = loads(tojson)
+lst = []
+for line in parsed:
+    print(line)
+    lst.append(line)
+rounds = 0
+while rounds != 10 and len(lst) >= 4:
+    dicright = dict()
+    vallist = []
+    n = random.randint(0, len(lst))
+    word = (lst[n])['Слово']
+    val = (lst[n])['Значение']
+    dicright[word] = val
+    vallist.append(val)
+    lst.remove(lst[n])
+    count = 0
+    while count != 3:
+        n = random.randint(0, len(lst))
+        val = (lst[n])['Значение']
+        vallist.append(val)
+        count += 1
+    random.shuffle(vallist)
+    choice = input() #пока не интегрирую в ботовские термины
+    if choice == dicright[word]:
+        round += 1
+    elif choice != dicright[word] and len(vallist) != 1:
+        vallist.remove(choice)
+        choice = input()
+    elif len(vallist) == 1:
+        round += 1
+    
