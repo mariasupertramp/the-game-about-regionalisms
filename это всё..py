ecf4883ce46979fae4e5efd108910f65362899f8
vargnorsk <107671948+vargnorsk@users.noreply.github.com>
2025-05-20 03:15:37 +0300

    Update это всё..py
    удалил черновую запись в конце файла, добавил lst на место (после парсинга)

--- это всё..py
+++ это всё..py
@@ -47,6 +47,9 @@ with open("local_words.json", "w", encoding="utf-8") as f:
 data = pd.read_json("local_words.json", orient="records")
 tojson = data.to_json(orient="records")
 parsed = loads(tojson)
+lst = []
+for line in parsed:
+    lst.append(line)
 
 @botik.message_handler(commands=['start'])
 def main(message):
@@ -104,32 +107,3 @@ def call_black(call):
     elif call.data == 'site':
         webbrowser.open('https://yandex.ru/company/researches/2021/local-words')
 botik.polling(none_stop=True)
-
-lst = []
-for line in parsed:
-    lst.append(line)
-rounds = 0
-while rounds != 10 and len(lst) >= 4:
-    dicright = dict()
-    vallist = []
-    n = random.randint(0, len(lst))
-    word = (lst[n])['Слово']
-    val = (lst[n])['Значение']
-    dicright[word] = val
-    vallist.append(val)
-    lst.remove(lst[n])
-    count = 0
-    while count != 3:
-        n = random.randint(0, len(lst))
-        val = (lst[n])['Значение']
-        vallist.append(val)
-        count += 1
-    random.shuffle(vallist)
-    choice = input() #пока не интегрирую в ботовские термины
-    if choice == dicright[word]:
-        round += 1
-    elif choice != dicright[word] and len(vallist) != 1:
-        vallist.remove(choice)
-        choice = input()
-    elif len(vallist) == 1:
-        round += 1
